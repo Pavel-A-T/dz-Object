f44ffe19ee2a3408dc1841721319d551d62f72ca
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-10-23 23:36:40 +0300

    first solve

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dz-Object
-Object, Reflection и Proxy
+Object, Reflection и Proxy [![Build status](https://ci.appveyor.com/api/projects/status/0dou6hmv9t2o74r2/branch/master?svg=true)](https://ci.appveyor.com/project/Pavel-A-T/dz-object/branch/master)
+
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import orderByProps from '../index';
+
+test(('function orderByProps'), () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const expected = [
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'attack', value: 80 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
+    // console.log(orderByProps(obj, ["defence", 'health']));
+  expect(orderByProps(obj, ['defence', 'health'])).toEqual(expected);
+});
